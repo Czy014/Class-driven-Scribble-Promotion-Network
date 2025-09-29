3f769320ba9365799cf0c0ea4e0ff7382d49e00b
Czy <jsjkxjczy@gmail.com>
2025-09-29 12:32:03 +0800

    添加一些调试信息和修改环境变量等信息

--- scripts/emlc_r50_deeplabv2.sh
+++ scripts/emlc_r50_deeplabv2.sh
@@ -1,13 +1,14 @@
 #/bin/bash
 
 # baseline no mask
-CUDA_VISIBLE_DEVICES=2 python ./evaluate.py \
+# CUDA_VISIBLE_DEVICES=2 
+python ./evaluate.py \
 --layers 50 \
 --dataset_path dataset/ScribbleSup/VOC2012 \
 --dataset VOC2012 \
 --numclasses 21 \
 --workers 2 \
---model_type res50_CAMATT \
+--model_type res50_lorm \
 --shrink_factor 1 \
 --checkpoint_path log/train_deeplabv2_r50/last_checkpoint.pth \
 --save_path emlc_r50_deeplabv2
